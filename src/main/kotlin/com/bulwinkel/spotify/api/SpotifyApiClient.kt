b9f5d70bdbe16b341caeae7ab25a82b31cfd1497
Bulwinkel <kelvin@bulwinkel.com>
2017-11-23 16:45:39 +1000

    Simplify api client and fix gradle to work with composite builds

--- src/main/kotlin/com/bulwinkel/spotify/api/SpotifyApiClient.kt
+++ src/main/kotlin/com/bulwinkel/spotify/api/SpotifyApiClient.kt
@@ -16,11 +16,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class SpotifyApiClient(
-    private val clientId: String,
-    private val clientSecret: String,
-    private val redirectUri: String,
-    private val authorizationCode: String,
-    private val credentialStore: CredentialStore = InMemoryCredentialsStore()
+    private val tokenSource: Single<String>
 ) {
 
   private val moshi:Moshi = Moshi.Builder().build()
@@ -37,61 +33,8 @@ class SpotifyApiClient(
       .baseUrl(SPOTIFY_API_BASE_URL)
       .build()
       .create(ApiService::class.java)
-  private val authService: AuthService = retrofitBuilder
-      .baseUrl(SPOTIFY_AUTH_BASE_URL)
-      .build()
-      .create(AuthService::class.java)
 
-  private val basicAuthHeader : String =
-      "Basic ${Base64.encodeToString("$clientId:$clientSecret".toByteArray(), Base64.NO_WRAP)}"
-
-  // expiresIn is in seconds
-  private fun calculatedExpiresAt(expiresIn: Int) : Long {
-    return System.currentTimeMillis() + ((expiresIn - 5) * 1000)
-  }
-
-  fun token() : Single<SpotifyCredentialsCache> {
-    return authService.token(
-        authHeader = basicAuthHeader,
-        redirectUri = redirectUri,
-        code = authorizationCode,
-        grantType = "authorization_code"
-    ).flatMap { token ->
-      val creds = SpotifyCredentialsCache(
-          accessToken = token.access_token,
-          tokenType = token.token_type,
-          scope = token.scope,
-          expiresAt = calculatedExpiresAt(token.expires_in),
-          refreshToken = token.refresh_token
-      )
-      return@flatMap credentialStore.save(creds)
-          .toSingleDefault(creds)
-    }
-  }
-
-  /**
-   * Single containing a valid auth header, checks if the token stored in the [CredentialStore]
-   * has expired and if so refreshes and store it before returning the refreshed value.
-   */
-  private val authHeader: Single<String> = credentialStore.load()
-      .flatMap { creds ->
-        if (creds.expiresAt <= System.currentTimeMillis()) {
-          authService.refreshToken(basicAuthHeader, creds.refreshToken)
-              .flatMap { refreshedToken ->
-                val updatedCredentials = creds.copy(
-                    accessToken = refreshedToken.access_token,
-                    tokenType = refreshedToken.token_type,
-                    scope = refreshedToken.scope,
-                    expiresAt = calculatedExpiresAt(refreshedToken.expires_in)
-                )
-                credentialStore.save(updatedCredentials)
-                    .toSingleDefault(updatedCredentials)
-              }
-        } else {
-          Single.just(creds)
-        }
-      }
-      .map { "${it.tokenType} ${it.accessToken}" }
+  private val authHeader: Single<String> get() = tokenSource.map { "Bearer $it" }
 
   /**
    * Get a list of the songs saved in the current Spotify user’s “Your Music” library.
