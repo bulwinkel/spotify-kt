f9ccc5e64191fd83b6abb48ee0129754e71a472e
Bulwinkel <kelvin@bulwinkel.com>
2017-12-13 15:29:09 +1000

    Add remaining args for auth and add browser prompt for auth code

--- spotify-cli-jvm/src/main/kotlin/com/bulwinkel/spotify/cli/auth/AuthCommand.kt
+++ spotify-cli-jvm/src/main/kotlin/com/bulwinkel/spotify/cli/auth/AuthCommand.kt
@@ -2,6 +2,8 @@ package com.bulwinkel.spotify.cli.auth
 
 import com.bulwinkel.spotify.cli.Command
 import fire.log.Fire
+import okhttp3.HttpUrl
+import java.util.Scanner
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -13,10 +15,56 @@ object AuthCommand : Command {
     override fun action(args: Array<String>) {
         Fire.d { "AuthCommand args: ${args.toList()}" }
 
-        val parsedArgs = AuthCommandArgs(args)
-        Fire.d { "parsedArgs.clientId = ${parsedArgs.clientId}" }
-        Fire.d { "parsedArgs.clientSecret = ${parsedArgs.clientSecret}" }
+        val code = AuthCommandArgs(args).run {
+            Fire.d { "clientId = $clientId" }
+            Fire.d { "clientSecret = $clientSecret" }
+            Fire.d { "scope = $scope" }
+            promptUserForToken(
+                    clientId = clientId,
+                    redirectUri = redirectUri,
+                    scope = scope
+            )
+        }
+
+        Fire.d { "authCode = $code" }
     }
+}
+
+private fun promptUserForToken(
+        clientId: String,
+        redirectUri: String,
+        scope: List<String> = emptyList(),
+        callbackUrl:String? = null
+) : String {
+
+    val urlBuilder = HttpUrl.Builder()
+            .scheme("https")
+            .host("accounts.spotify.com")
+            .addPathSegment("authorize")
+            .addQueryParameter("client_id", clientId)
+            .addQueryParameter("response_type", "code")
+            .addQueryParameter("redirect_uri", redirectUri)
+            .addQueryParameter("show_dialog", true.toString())
+
+    val scope = if (scope.isNotEmpty()) scope.joinToString(" ") else null
+    if (scope != null) {
+        urlBuilder.addQueryParameter("scope", scope)
+    }
+
+    val actualCallbackUrl = if (callbackUrl != null) {
+        callbackUrl
+    } else {
+        val url = urlBuilder.build()
+        Runtime.getRuntime().exec("open $url")
+        println("enter the callback url displayed in the browser:")
+        val scanner = Scanner(System.`in`)
+        scanner.next()
+    }
+
+    val code = actualCallbackUrl.split(delimiters = "code=").last().split(delimiters = "&").first()
+    println("code = $code")
+
+    return code
 }
 
 class AuthCommandArgs(args: Array<String>) {
@@ -28,28 +76,37 @@ class AuthCommandArgs(args: Array<String>) {
         }
     }
 
-    val clientId by StringArg(args, readLineOrThrow("Enter Client ID:"))
-    val clientSecret by StringArg(args, readLineOrThrow("Enter Client Secret:"))
-    val redirectUri by StringArg(args, readLineOrThrow("Enter RedirectUri:"))
-
+    val clientId by stringArg(args, readLineOrThrow("Enter Client ID:"))
+    val clientSecret by stringArg(args, readLineOrThrow("Enter Client Secret:"))
+    val redirectUri by stringArg(args, readLineOrThrow("Enter RedirectUri:"))
+    val scope by arg(args, { emptyList<String>() }) { it.split(",")}
 }
 
+private val stringValueTransformer: (String) -> String = { it }
 
+fun stringArg(args: Array<String>, defaultValue: (argName: String) -> String) : ArgDelegate<String> {
+    return ArgDelegate(args, defaultValue, stringValueTransformer)
+}
 
-class StringArg(
+fun <T> arg(
+        args: Array<String>,
+        defaultValue: (argName: String) -> T,
+        valueTransformer: (value: String) -> T
+) = ArgDelegate(args, defaultValue, valueTransformer)
+
+class ArgDelegate<out T>(
         private val args: Array<String>,
-        private val defaultValue: (argName: String) -> String = {
-            throw NullPointerException("arg $it must be provided")
-        }
-) : ReadOnlyProperty<AuthCommandArgs, String> {
+        private val defaultValue: (argName: String) -> T,
+        private val valueTransformer: (value: String) -> T
+) : ReadOnlyProperty<AuthCommandArgs, T> {
 
-    private var value: String? = null
+    private var value: T? = null
 
-    override fun getValue(thisRef: AuthCommandArgs, property: KProperty<*>): String {
+    override fun getValue(thisRef: AuthCommandArgs, property: KProperty<*>): T {
         if (value == null) {
             val propName = property.name
             Fire.d {  "looking up value for property.name = $propName" }
-            value = args.findValueOrNull("--$propName") ?: defaultValue(propName)
+            value = args.findValueOrNull("--$propName")?.let(valueTransformer) ?: defaultValue(propName)
         }
 
         return value!!
