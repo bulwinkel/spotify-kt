6dbd5f29e3c3693f774128ebd73d354b986997e3
Bulwinkel <kelvin@bulwinkel.com>
2018-10-21 17:18:51 +1000

    Move dependency declarations into buildsrc

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,8 @@
+object Deps {
+    val kotlin_version = "1.3.0-rc-190"
+    val ktor_version = "1.0.0-beta-1"
+    val serialization_version = "0.8.3-rc13"
+    val coroutines_version = "1.0.0-RC1"
+    val moshiVersion = "1.6.0"
+    val retrofitVersion = "2.4.0"
+}
